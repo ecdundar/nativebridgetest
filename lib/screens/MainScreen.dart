edc35a136345663f4d0a48b0a8d99203e9b641f3
Eyüp Can Dündar <eyup@ecdyazilim.com>
2023-03-02 09:42:46 +0300

    Buton eklendi

--- lib/screens/MainScreen.dart
+++ lib/screens/MainScreen.dart
@@ -12,7 +12,14 @@ class _MainScreenState extends State<MainScreen> {
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(title: const Text('Native Bridge Test')),
-      body: Container(color: Colors.yellow, child: Center(child: Container())),
+      body: Container(
+        color: Colors.yellow,
+        child: Center(
+            child: ElevatedButton.icon(
+                onPressed: () {},
+                icon: const Icon(Icons.open_in_browser),
+                label: const Text('Browser Aç'))),
+      ),
     );
   }
 }
